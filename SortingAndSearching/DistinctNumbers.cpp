c1dbf8ea0cc525e96eaf744c330dc021a1c1414d
Debdeep <dbworkofficial@gmail.com>
2025-09-18 08:41:51 +0530

    now

---
+++ SortingAndSearching/DistinctNumbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+
+    set<int> st(v.begin(), v.end());
+
+    cout << st.size();
+    return 0;
+}
